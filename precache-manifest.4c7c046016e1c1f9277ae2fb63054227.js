da4dfcebea1afdd038d882592a6e8dc21d78d7ea
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-03-30 12:17:11 -0700

    Updates

---
+++ precache-manifest.4c7c046016e1c1f9277ae2fb63054227.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "465ce3d70b84c2a368f3",
+    "url": "/Sticky-Notes/static/css/main.a2202385.chunk.css"
+  },
+  {
+    "revision": "465ce3d70b84c2a368f3",
+    "url": "/Sticky-Notes/static/js/main.aa6906b5.chunk.js"
+  },
+  {
+    "revision": "b15224541b42275ca52a",
+    "url": "/Sticky-Notes/static/js/runtime~main.c1a6b2e5.js"
+  },
+  {
+    "revision": "59fa5faa0d43ca05c280",
+    "url": "/Sticky-Notes/static/js/2.da757658.chunk.js"
+  },
+  {
+    "revision": "d0916bf013351854ac412e52d9f47406",
+    "url": "/Sticky-Notes/index.html"
+  }
+];
